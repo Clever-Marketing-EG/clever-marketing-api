c952df4ab75a4b7c2319343873bef90c0f5de29f
Raymond Youssef <rimonomega@gmail.com>
2021-04-12 16:17:16 +0200

    done with project controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,62 +3,90 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class ProjectController extends Controller
+class ProjectController extends ResourcesController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $projects
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $validated = Project::validate($request);
+        $project = Project::create($validated);
+
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
+     * @param Project $project
+     * @return JsonResponse
      */
-    public function show(Project $project)
+    public function show(Project $project): JsonResponse
     {
-        //
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Project $project
+     * @return JsonResponse
      */
-    public function update(Request $request, Project $project)
+    public function update(Request $request, Project $project): JsonResponse
     {
-        //
+        $validated = Project::validate($request);
+        $project->update($validated);
+
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
+     * @param Project $project
+     * @return JsonResponse
+     * @throws Exception
      */
-    public function destroy(Project $project)
+    public function destroy(Project $project): JsonResponse
     {
-        //
+        $project->delete();
+
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
     }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Project extends Model
 {
     use HasFactory;
+
+    /**
+     * Validate Project instance
+     *
+     * @param Request $request
+     * @return array
+     */
+    public static function validate(Request $request): array
+    {
+        return $request->validate([
+            'image_url' => 'url',
+            'type' => 'required|in:web,branding,graphics'
+        ]);
+    }
 }
