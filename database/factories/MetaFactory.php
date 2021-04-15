29ae3b327d381457b0e10b4f8cc5a64b4b2c1f9d
Rana Medhat <rana.medhat69@gmail.com>
2021-04-15 12:07:51 +0200

    Added Meta

--- database/factories/MetaFactory.php
+++ database/factories/MetaFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Meta;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
 
 class MetaFactory extends Factory
 {
@@ -19,15 +21,71 @@ class MetaFactory extends Factory
      *
      * @return array
      */
-    public function definition(): array
+    public function definition()
     {
-        return [
-            'position'=> 'Home Page Header 1',
-            'name' => $this->faker->word,
-            'content' => $this->faker->text,
-            'content_ar' => 'محتوى',
-            'type' => 'text',
-            'page' =>  $this->faker->randomElement(['home', 'about-us', 'contact-us'])
-        ];
+            $contactData = array(
+                [   'position' => 'Contact Page' ,
+                    'name' => 'contact_phone',
+                    'content' => 'Phone',
+                    'content_ar' => 'محتوى',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ],
+                [
+                    'position' => 'Contact Page' ,
+                    'name' => 'contact_number',
+                    'content' => '+20123456789',
+                    'content_ar' => '+20123456789',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ],
+                [
+                    'position' => 'Contact Page' ,
+                    'name' => 'contact_email',
+                    'content' => 'Email',
+                    'content_ar' => 'محتوى',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ],
+                [
+                    'position' => 'Contact Page' ,
+                    'name' => 'contact_mail',
+                    'content' => 'Email',
+                    'content_ar' => 'example@example.com',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ],
+                [
+                    'position' => 'Contact Page' ,
+                    'name' => 'contact_address',
+                    'content' => 'Address',
+                    'content_ar' => 'محتوى',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ],
+                [
+                    'position' => 'Contact Page' ,
+                    'name' => 'contact_location',
+                    'content' => 'Cairo,Egypt',
+                    'content_ar' => 'مصر',
+                    'type' => 'text',
+                    'page' => 'contact'
+                ]
+            );
+            $footerData = array(
+                [
+                    'position' => 'Footer' ,
+                    'name' => 'content',
+                    'content' => 'Footer Content !',
+                    'content_ar' => 'محتوي',
+                    'type' => 'text',
+                    'page' => 'footer'
+                ],
+            );
+
+            $data = array_merge($contactData, $footerData);
+
+            DB::table('metas')->insert($data);
+
     }
 }
