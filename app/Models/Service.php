0ad17100c74d2885965c08b97574f0a399fca196
Raymond Youssef <rimonomega@gmail.com>
2021-04-18 11:34:03 +0200

    small fix

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -15,8 +15,7 @@ class Service extends Model
 
     protected $casts = [
         'points' => 'array',
-        'points_ar' => 'array',
-        'images' => 'array'
+        'points_ar' => 'array'
     ];
 
     public $timestamps = false;
